94235335a7a8ba832e5c0f7f16709d1a66239a25
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 12:48:39 +0530

    Got the CREDENTIALS from environment varriable.

--- route/Dialogflow.js
+++ route/Dialogflow.js
@@ -3,6 +3,8 @@ const dialog = express();
 const dialogflow = require('@google-cloud/dialogflow');
 require('dotenv').config();
 
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
 dialog.get('/', (req, res) => {
     res.json('You are in dialogflow module.')
 })
