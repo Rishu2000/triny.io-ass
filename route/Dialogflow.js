1084b3f7874eaff4e5239a0f91a63138481d8782
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 12:47:13 +0530

    Implemented the dialogflow and dotenv packages.

--- route/Dialogflow.js
+++ route/Dialogflow.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dialog = express();
+const dialogflow = require('@google-cloud/dialogflow');
+require('dotenv').config();
 
 dialog.get('/', (req, res) => {
     res.json('You are in dialogflow module.')
