6a947ec723d848f2b9c9abbdefb09db6d024a96e
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 15:23:46 +0530

    implemented axios.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,19 @@
 import React, {useState} from 'react'
 import "../styles/App.css"
+import axios from 'axios'
 
 const App = () => {
 
 const [queryText, setQueryText] = useState(null);
 const [sessionId, setSessionId] = useState(null);
+const [dialogRes, setDialogRes] = useState(null);
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  console.log(queryText, sessionId);
+  axios.post('http://localhost:4002/dialogflow',{queryText,sessionId})
+  .then((res) => {
+    setDialogRes(res.data);
+  })
 }
 
   return (
@@ -23,6 +28,13 @@ const handleSubmit = (e) => {
         </div>
         <button type="submit" className="btn btn-primary" style={{marginTop:20}}>Submit</button>
       </form>
+      {dialogRes && (
+        <div style={{display: 'flex', justifyContent: 'center', marginTop:30}}>
+          <div className="alert alert-info" style={{width:"50%"}}>
+            {dialogRes}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
