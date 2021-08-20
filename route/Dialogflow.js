90dc422b713ce3c5a4a9626c581c271e5084d405
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 13:03:41 +0530

    detect Intent method complete.

--- route/Dialogflow.js
+++ route/Dialogflow.js
@@ -16,6 +16,23 @@ const CONFIGURATION = {
 
 const sessionClient = new dialogflow.SessionsClient(CONFIGURATION);
 
+const detectIntent = async (languageCode, queryText, sessionId) => {
+    let sessionPath = sessionClient.projectAgentSessionPath(PROJECTID,sessionId);
+
+    //The text query request.
+    let request = {
+        session:sessionPath,
+        queryInput:{
+            text:{
+                //The query to send to the dialogflow agent
+                text:queryText,
+                //The language used by client
+                languageCode:languageCode,
+            }
+        }
+    }
+}
+
 dialog.get('/', (req, res) => {
     res.json('You are in dialogflow module.')
 })
