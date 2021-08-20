63aad3cb62269e5f617e6139786c543741fa43d4
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 12:56:34 +0530

    Configuration for the client.

--- route/Dialogflow.js
+++ route/Dialogflow.js
@@ -7,6 +7,12 @@ const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 
 const PROJECTID = CREDENTIALS.project_id;
 
+const CONFIGURATION = {
+    credentials:{
+        private_key: CREDENTIALS['private_key'],
+        client_email: CREDENTIALS['client_email'],
+    }
+}
 dialog.get('/', (req, res) => {
     res.json('You are in dialogflow module.')
 })
