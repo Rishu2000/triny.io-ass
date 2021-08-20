9e1f156de79e3c32d6c3c19ce866b206629e3700
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 15:23:29 +0530

    Implemented cors.

--- Server.js
+++ Server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT ||4002;
 const dialogflowData = require('./route/Dialogflow');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/dialogflow',dialogflowData);
