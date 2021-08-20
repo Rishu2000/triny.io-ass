ebd083cd7b3ac96ac675951077c760f09a905127
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 12:55:38 +0530

    Get my google dialogflow projectID.

--- route/Dialogflow.js
+++ route/Dialogflow.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 
 const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 
+const PROJECTID = CREDENTIALS.project_id;
+
 dialog.get('/', (req, res) => {
     res.json('You are in dialogflow module.')
 })
