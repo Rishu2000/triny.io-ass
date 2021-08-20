f512aa91d034ac80bd051f18b2e46c8173a2936d
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-20 14:59:34 +0530

    Created the form and handle the submit.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../styles/App.css"
 
 const App = () => {
+
+const [queryText, setQueryText] = useState(null);
+const [sessionId, setSessionId] = useState(null);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(queryText, sessionId);
+}
+
   return (
-    <div>
-      Rishav
+    <div style={{textAlign: 'center', paddingTop:80}}>
+      Please enter <strong style={{fontSize:25}}>Start</strong> as queryText
+      <form onSubmit={handleSubmit} style={{paddingTop:20}}>
+        <div>
+          <input type="text" style={{margin:"0 10px", padding:"0 10px"}} 
+            placeholder="queryText" onChange={(e) => setQueryText(e.target.value)}/>
+          <input type="text" style={{margin:"0 10px", padding:"0 10px"}} 
+            placeholder="sessionId" onChange={(e) => setSessionId(e.target.value)}/>
+        </div>
+        <button type="submit" className="btn btn-primary" style={{marginTop:20}}>Submit</button>
+      </form>
     </div>
   )
 }
